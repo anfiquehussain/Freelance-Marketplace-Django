ee28d933cf9ec7d60de43379a2f15ccb8088ca3e
anfique hussain v <anfiquehussain1@gmail.com>
2023-10-08 20:48:11 +0530

    rezorpay add-order transtion worked,windrawal not

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ myworld\Scripts\activate.bat
 myenv\Scripts\activate.bat
 
  <!-- {% if user_profile.user.username and user_profile.user.email and user_profile.user.first_name and user_profile.user.last_name and user_profile.country and user_profile.state and user_profile.website_link and user_profile.about_me  and user_profile.skills.all and user_profile.certifications.all and user_profile.languages.all %} -->
+
+         <!-- <a id="openButton" href="{% url 'submit_requirements' transaction_id request.user.username %}"
+            class="btn btn-primary btn-lg">SUBMIT REQUIREMENTS</a> -->
+
+
--- payments/views.py
+++ payments/views.py
@@ -14,11 +14,10 @@ from datetime import timedelta
 from django.utils import timezone
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import redirect
+import razorpay
+from django.http import HttpResponseBadRequest
 
-
-# import razorpay
-
-# client = razorpay.Client(auth=(settings.REZORPAY_PUBLISHABLE_KEY, settings.REZORPAY_SECRET_KEY))
+client = razorpay.Client(auth=(settings.REZORPAY_PUBLISHABLE_KEY, settings.REZORPAY_SECRET_KEY))
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -55,33 +54,25 @@ def payments(request, overview_id,username):
 
     if additional_data2 == '1':
         for bp in basic_packages:
-            # data = { "amount": bp.Basic_price * 100, "currency": "INR", "receipt": "order_rcptid_11" }
+
+            
             
             bayer_fee = bp.Basic_price * (5/100)
             seller_fee = bp.Basic_price * (20/100)
             service_fee = buyer_fee + seller_fee
+
+           
+
+            price = (bp.Basic_price + bayer_fee)
             
-            price = (bp.Basic_price + seller_fee) * 100
             actual_price = bp.Basic_price
             actual_price_fee_added = bp.Basic_price + seller_fee
 
             
-            payment_intent = stripe.PaymentIntent.create(
-                amount=int(actual_price_fee_added * 100),
-                currency='inr',
-                payment_method_types=['card'],
-                description=bp.Basic_description,
-                receipt_email=request.user.email,
-                
-                 
-                metadata={
-                    'overview_id': overview.id,
-                    'package_name': 'basic_package',
-                }
-            )
-            payment_id = payment_intent.id
-            
-            
+            data = { "amount": price * 100, "currency": "INR", "receipt": "order_rcptid_11" }
+            payment = client.order.create(data=data)
+            API_KRY = settings.REZORPAY_PUBLISHABLE_KEY
+            order_id = payment['id']
             
             package_discription = bp.Basic_description
 
@@ -90,79 +81,82 @@ def payments(request, overview_id,username):
                 sender=request.user,
                 receiver=user_profile.user,
                 amount=actual_price,
-                payment_id=payment_id,
+                payment_id=order_id,
                 package_name='basic_package',
                 service_fee=service_fee
             )
             transaction_id = transaction.id  
     elif additional_data2 == '2':
-        for sp in standard_packages:
-
-            bayer_fee = sp.Standard_price * (5/100)
-            seller_fee = sp.Standard_price * (20/100)
+        for bp in standard_packages:
+            bayer_fee = bp.Standard_price * (5/100)
+            seller_fee = bp.Standard_price * (20/100)
             service_fee = buyer_fee + seller_fee
 
-            price = (sp.Standard_price + seller_fee) * 100
-            actual_price = sp.Standard_price
-            actual_price_fee_added = sp.Standard_price + seller_fee
+           
 
-            payment_intent = stripe.PaymentIntent.create(
-                amount=int(price),
-                currency='inr',
-                payment_method_types=['card']
-            )
-            payment_id = payment_intent.id
-            package_discription = sp.Standard_description
+            price = (bp.Standard_price + bayer_fee)
+            
+            actual_price = bp.Standard_price
+            actual_price_fee_added = bp.Standard_price + seller_fee
+
+            
+            data = { "amount": price * 100, "currency": "INR", "receipt": "order_rcptid_11" }
+            payment = client.order.create(data=data)
+            API_KRY = settings.REZORPAY_PUBLISHABLE_KEY
+            order_id = payment['id']
+            
+            package_discription = bp.Standard_description
 
             transaction = Transaction.objects.create(
                 overview=overview.id,
                 sender=request.user,
                 receiver=user_profile.user,
                 amount=actual_price,
-                payment_id=payment_id,
+                payment_id=order_id,
                 package_name='standard_package',
                 service_fee=service_fee
             )
-            transaction_id = transaction.id
-    elif additional_data2 == '3':
-        for pp in premium_packages:
+            transaction_id = transaction.id  
 
-            bayer_fee = pp.Premium_price * (5/100)
-            seller_fee = pp.Premium_price * (20/100)
+    elif additional_data2 == '3':
+        for bp in premium_packages:
+            bayer_fee = bp.Premium_price * (5/100)
+            seller_fee = bp.Premium_price * (20/100)
             service_fee = buyer_fee + seller_fee
 
-            price = (pp.Premium_price + seller_fee) * 100
-            actual_price = pp.Premium_price
-            actual_price_fee_added = pp.Premium_price + seller_fee
+           
 
-            print(actual_price_fee_added)
-            payment_intent = stripe.PaymentIntent.create(
-                amount=int(price),
-                currency='inr',
-                payment_method_types=['card']
-            )
-            payment_id = payment_intent.id
-            package_discription = pp.Premium_description
+            price = (bp.Premium_price + bayer_fee)
+            
+            actual_price = bp.Premium_price
+            actual_price_fee_added = bp.Premium_price + seller_fee
+
+            
+            data = { "amount": price * 100, "currency": "INR", "receipt": "order_rcptid_11" }
+            payment = client.order.create(data=data)
+            API_KRY = settings.REZORPAY_PUBLISHABLE_KEY
+            order_id = payment['id']
+            
+            package_discription = bp.Premium_description
 
             transaction = Transaction.objects.create(
                 overview=overview.id,
                 sender=request.user,
                 receiver=user_profile.user,
                 amount=actual_price,
-                payment_id=payment_id,
+                payment_id=order_id,
                 package_name='premium_package',
                 service_fee=service_fee
             )
-            transaction_id = transaction.id
+            transaction_id = transaction.id 
+
             
             
 
-    # payment = client.order.create(data=data)
-    # API_KRY = settings.REZORPAY_PUBLISHABLE_KEY
-    # order_id = payment['id']
 
-   
-    print(package_discription)
+
+
+    print(payment['id'])
         
     context = {
         'user':user,
@@ -177,126 +171,65 @@ def payments(request, overview_id,username):
         'package_discription':package_discription,
         'service_fee' :  service_fee,
         'actual_price_fee_added':actual_price_fee_added,
-
-        # 'payment' : payment,
-        # 'API_KRY' : API_KRY,
-        # 'order_id' : order_id,
+        'order_id':order_id,
+        'payment' : payment,
+        'API_KRY' : API_KRY,
+        'order_id' : order_id,
     }
     return render(request, 'payment.html', context)
 
 @login_required()
-def success(request, transaction_id,username):
+def success(request, transaction_id, username):
     transaction = get_object_or_404(Transaction, id=transaction_id)
     user = get_object_or_404(User, username=username)
-    
-    
-
     current_user = request.user
+    seller_user = None  
+    context = {} 
 
     if username == current_user.username and transaction.sender == current_user:
         pass
     else:
         return HttpResponseForbidden("Access Denied")
-        
-    payment_intent = stripe.PaymentIntent.retrieve(transaction.payment_id)
+
+    transaction.payment_status = True
+    overview = transaction.overview
+    overview_id = Overview.objects.get(pk=overview)
+    transaction.save()
     
-
-    
-    try:
-        # Retrieve the PaymentIntent
-        payment_intent = stripe.PaymentIntent.retrieve(transaction.payment_id)
-        
-
-        # Replace 'tok_12345' with the actual token generated on the client-side
-        stripe_token = request.POST.get('stripeToken')
-        
-        payment_method = stripe.PaymentMethod.create(
-            type="card",
-            card={
-                "token": stripe_token
-            }
-        )
-
-        # The payment method ID is available as payment_method.id
-        actual_payment_method_id = payment_method.id
-        stripe_url_webhook = None
-        if payment_intent.status == 'requires_payment_method':
-            # Modify the PaymentIntent with the new payment method
-            payment_intent = stripe.PaymentIntent.modify(
-                payment_intent.id,
-                payment_method=actual_payment_method_id
-            )
-
+    if transaction.package_name == "basic_package":
+        basic_package = BasicPackage.objects.get(overview=overview_id)
+        Basic_delivery_time = basic_package.Basic_delivery_time
+        delivery_date = timezone.now() + timedelta(days=Basic_delivery_time)
             
-            # Confirm the PaymentIntent
-            payment_intent.confirm()
-            transaction.payment_status = True
-            overview = transaction.overview
-            overview_id = Overview.objects.get(pk=overview)
-            transaction.save()
-
-            if transaction.package_name == "basic_package":
-                basic_package = BasicPackage.objects.get(overview=overview_id)
-                Basic_delivery_time = basic_package.Basic_delivery_time
-                delivery_date = timezone.now() + timedelta(days=Basic_delivery_time)
+    elif transaction.package_name == "standard_package":
+        standard_package = StandardPackage.objects.get(overview=overview_id)
+        Standard_delivery_time = standard_package.Standard_delivery_time
+        delivery_date = timezone.now() + timedelta(days=Standard_delivery_time)
             
-            elif transaction.package_name == "standard_package":
-                standard_package = StandardPackage.objects.get(overview=overview_id)
-                Standard_delivery_time = standard_package.Standard_delivery_time
-                delivery_date = timezone.now() + timedelta(Standard_delivery_time)
-            
-            elif transaction.package_name == "premium_package":
-                premium_package = PremiumPackage.objects.get(overview=overview_id)
-                Premium_delivery_time = premium_package.Premium_delivery_time
-                delivery_date = timezone.now() + timedelta(Premium_delivery_time)
+    elif transaction.package_name == "premium_package":
+        delivery_date =None
+        premium_package = PremiumPackage.objects.get(overview=overview_id)
+        premium_delivery_time = premium_package.Premium_delivery_time
+        delivery_date = timezone.now() + timedelta(days=premium_delivery_time)
 
-            seller_user = transaction.receiver 
-            
-            order = Order.objects.create(
-                buyer=current_user,
-                service=overview_id,
-                status='pending',
-                transaction=transaction,
-                delivery_date=delivery_date,
-                seller=seller_user,
-            )
+    seller_user = transaction.receiver 
+    print(delivery_date)
 
+    order = Order.objects.create(
+        buyer=current_user,
+        service=overview_id,
+        status='pending',
+        transaction=transaction,
+        delivery_date=delivery_date,
+        seller=seller_user,
+    )
 
-            print(f"PaymentIntent confirmed. Status: {payment_intent.status}")
-        else:
-            print(f"PaymentIntent status: {payment_intent.status}")
-        
-
-        if payment_intent.status == 'requires_action' and 'next_action' in payment_intent:
-            next_action = payment_intent['next_action']
-
-           
-            if next_action.get('type') == 'use_stripe_sdk':
-
-                source = next_action['use_stripe_sdk']['source']
-                stripe_js_url = next_action['use_stripe_sdk']['stripe_js']
-               
-                stripe_url_webhook = stripe_js_url
-                
-
-            # return redirect(stripe_js_url)
-
-    except stripe.error.StripeError as e:
-        # Handle Stripe API errors
-        print(f"Stripe error: {e}")
-    except Exception as e:
-        # Handle other exceptions
-        print(f"An error occurred: {e}")
-    
-
-
-   
     context = {
-        'transaction_id' : transaction_id,
-        'stripe_url_webhook':stripe_url_webhook,
-        
-    }
-    return render(request, 'package_selection.html',context)
+            'transaction_id': transaction_id,
+        }
+
+    return render(request, 'package_selection.html', context)
+
 
 
 
@@ -307,84 +240,30 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-# views.py
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
-import stripe
+import razorpay
 
-# Set the Stripe API key from settings
-stripe.api_key = settings.STRIPE_SECRET_KEY
+import requests
 
 @login_required
 def withdrawal(request, username):
-    user = get_object_or_404(User, username=username)
+    client = razorpay.Client(auth=(settings.REZORPAY_PUBLISHABLE_KEY, settings.REZORPAY_SECRET_KEY))
+    account_data = {
+    "type": "bank_account",  # Specify the type of account (e.g., "bank_account")
+    "contact": {
+        "name": "John Doe",  # Name of the account holder
+        "email": "john.doe@example.com",  # Email address of the account holder
+        "phone": "1234567890",  # Phone number of the account holder
+    },
+    "bank_account": {
+        "account_number": "1234567890",  # Account number
+        "ifsc": "ABCD1234567",  # IFSC code of the bank branch
+        "beneficiary_name": "John Doe",  # Name of the account holder (again)
+        },
+    }
+    linked_account = client.account.create(data=account_data)
 
-    # bank_account_token = stripe.Token.create(
-    #     bank_account={
-    #         'country': 'IN',
-    #         'currency': 'inr',
-    #         'account_holder_name': 'Test User',
-    #         'account_holder_type': 'individual',
-    #         'routing_number': 'HDFC0000261',
-    #         'account_number': '000123456789',
-    #     },
-    # )
-
-    connected_account_id = 'acct_1Nl4VZSBLWrgjYEW'
-
-    try:
-        bank_account_token = stripe.Token.create(
-            bank_account={
-                'country': 'IN',
-                'currency': 'inr',
-                'account_holder_name': "test user",
-                'account_holder_type': "individual",
-                'routing_number': 'HDFC0000261',
-                'account_number': '000123456789',
-            },
-            stripe_account='acct_1Nl4VZSBLWrgjYEW',
-        )
-
-        # connected_account = stripe.Account.create(
-        #     type="express",
-        #     country="IN",
-        #     external_account=bank_account_token.id,
-        # )
-        transfer = stripe.Transfer.create(
-            amount=900,
-            currency='inr',
-            destination='acct_connected_account_id',
-        )
-
-
-
-
-
-
-
-# Get the ID of the connected account
-        # connected_account_id = connected_account.id
-        # print(connected_account)
-
-        # payout = stripe.Payout.create(
-        #     amount=int(100 * 100),
-        #     currency='inr',
-        #     method='express',
-        #     destination=bank_account_token.id,
-        # )
-
-        
-        if payout_to_bank.status == 'paid':
-            print(f"Payout to bank successful. Payout ID: {payout_to_bank.id}")
-        else:
-            print(f"Payout to bank failed. Error: {payout_to_bank.failure_message}")
-
-    except stripe.error.StripeError as e:
-        messages.error(request, f"Stripe Error: {e}")
-        print("Stripe Error:", e)
-
+    linked_accounts = client.account.create()
     return render(request, "withdrawal.html")
-
     
 
 
