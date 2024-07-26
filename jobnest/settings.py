1d0924b2ead28a3b2694deee21d1c64ed509918e
anfique <anfiquehussain1@gmail.com>
2024-07-26 23:41:12 +0530

    diploy test 10 vercel(change the vercel.json path)

--- jobnest/settings.py
+++ jobnest/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-36^+27+m=m5qow@cit20@)m3-t35hq9x-3g==e%x5&xtvn+#s5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG'),
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
